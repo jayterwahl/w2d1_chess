adb862105e5c529852d8f6969c2c030ce480ccac
App Academy Student <student@appacademy.io>
2016-01-11 15:43:31 -0800

    Added move method.

--- board.rb
+++ board.rb
@@ -6,4 +6,15 @@ class Board
     @grid = Array.new(8){Array.new(8){Nul_piece.new}}
   end
 
+  #assume start & end pos == array for now
+  def move(start_pos,end_pos)
+    selected_piece = @grid[start_pos[0]][start_pos[1]]
+    selected_destination = @grid[end_pos[0]][end_pos[1]]
+    if selected_piece.is_a?(Nul_piece)
+      raise NoPieceError.new "There's no piece at that location."
+      #also something for selecting opponent's pieces
+    end
+
+  end
+
 end
